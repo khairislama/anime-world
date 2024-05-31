08cac637c9fa884a02748fd172224baca5c27c1c
Khairi SLAMA <khairi.slama.1996.br@gmail.com>
2024-06-01 00:00:48 +0100

    feat: :sparkles: Framer motion + return jsx from server action. 'END OF YOUTUBE VIDEO'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
+      </body>
     </html>
   );
 }
